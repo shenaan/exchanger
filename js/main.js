b98b4d4347a631673906ea8e2efb17d5c9060994
shenaan <shenaan@gmail.com>
2017-10-07 01:44:48 +0300

    added header bg and body bg animation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
 $(document).ready(function () {
+
+    //header modal
+
    $("#btn-mob").click(function (e) {
        var $headerModal = $('#header-modal');
        e.preventDefault();
        $(this).toggleClass('active');
        $headerModal.toggleClass('active');
-   })
+   });
 
     $(document).on('click',function(e){
         if(!$(e.target).closest('#btn-mob,#header-modal').length) {
             $('#btn-mob,#header-modal').removeClass('active');
         }
     });
+
+   //range slider
+
+    $( "#give-slider" ).slider({
+        range: false,
+        min: 0,
+        max: 50000,
+        values: [17599],
+        slide: function( event, ui ) {
+            $( "#give-amount" ).val( ui.values[ 0 ]);
+        }
+    });
+    $( "#give-amount" ).val( $( "#give-range" ).slider( "values", 0 ));
+
+
+    $('.give-options').styler();
+
 
 });
